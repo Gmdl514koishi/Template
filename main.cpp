600c9c8ff7d468aa0389ad19f3242bac6135d507
Gmdl514koishi <3536512649@qq.com>
2026-03-08 20:48:53 +0800

    重构 DisJointSet 类，添加并实现 find_root、unite 和 is_same 方法

--- main.cpp
+++ main.cpp
@@ -4,6 +4,42 @@
 #include <tuple>
 #include <vector>
 
+class DisJointSet {
+private:
+    std::vector<int> root_node_and_depth;
+public:
+    DisJointSet(const int n) : root_node_and_depth(n, -1) {}
+    int find_root(int x) {
+        int root = x;
+        while (root_node_and_depth[root] >= 0) {
+            root = root_node_and_depth[root];
+        }
+        while (x != root) {
+            const int next = root_node_and_depth[x];
+            root_node_and_depth[x] = root;
+            x = next;
+        }
+        return root;
+    }
+    void unite(int x, int y) { 
+        x = find_root(x);
+        y = find_root(y);
+        const unsigned int x_depth = root_node_and_depth[x];
+        const unsigned int y_depth = root_node_and_depth[y];
+        if (x_depth > y_depth) {
+            root_node_and_depth[y] = x;
+        } else if (x_depth < y_depth) {
+            root_node_and_depth[x] = y;
+        } else if (x != y) {
+            root_node_and_depth[y] = x;
+            --root_node_and_depth[x];
+        }
+    }
+    bool is_same(int x, int y) {
+        return find_root(x) == find_root(y);
+    }
+};
+
 class UndirectedMinimumSpanningTree {
 private:
     struct Comparator {
@@ -51,42 +87,6 @@ public:
             return -1;
         }
         return cost;
-    }
-};
-
-class DisJointSet {
-private:
-    std::vector<int> root_node_and_depth;
-public:
-    DisJointSet(const int n) : root_node_and_depth(n, -1) {}
-    int find_root(int x) {
-        int root = x;
-        while (root_node_and_depth[root] >= 0) {
-            root = root_node_and_depth[root];
-        }
-        while (x != root) {
-            const int next = root_node_and_depth[x];
-            root_node_and_depth[x] = root;
-            x = next;
-        }
-        return root;
-    }
-    void unite(int x, int y) { 
-        x = find_root(x);
-        y = find_root(y);
-        const unsigned int x_depth = root_node_and_depth[x];
-        const unsigned int y_depth = root_node_and_depth[y];
-        if (x_depth > y_depth) {
-            root_node_and_depth[y] = x;
-        } else if (x_depth < y_depth) {
-            root_node_and_depth[x] = y;
-        } else if (x != y) {
-            root_node_and_depth[y] = x;
-            --root_node_and_depth[x];
-        }
-    }
-    bool is_same(int x, int y) {
-        return find_root(x) == find_root(y);
     }
 };
 
